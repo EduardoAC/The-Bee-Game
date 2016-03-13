59f69ff4c8c40fd46ce1854cb7ffa1c456763b6f
EduardoAC <eduardo@dreammakerfactory.com>
2016-03-13 14:36:17 +0000

    Put logic in the hive view

--- game.js
+++ game.js
@@ -3,45 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
-
-var getRandomMinMax = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-var printStatus = function (statusText, className) {
-    var logGame = document.getElementById("log-game");
-    var status = document.createElement("LI");
-    var statusNodeText = document.createTextNode(statusText);
-    status.appendChild(statusNodeText);
-    status.className = className;
-    logGame.appendChild(status);
-
-};
-
 var domReady = function (callback) {
     document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
 };
 
 var beeNest;
 
-var repaint = function () {
-    //Draw Game layout
-    var canvas = document.getElementById("game-console");
-    var context = drawHexGrid({}, canvas);
-    var length = beeNest.length;
-
-    for (var i = 0; i < length; i++) {
-        beeNest[i].render(context);
-    }
-};
-
 var initializeGame = function () {
-    beeNest = new Hive();
+    beeNest = new Hive("game-console");
     beeNest.createHive();
-    repaint();
+    beeNest.events();
+    beeNest.render();
 };
 
 var resetGame = function () {
@@ -56,29 +28,6 @@ var initialize = function () {
 
     //Build the Nest with 5 workers, 8 Drones and one Queen
     initializeGame();
-
-    //Manage the firing button to kill he Nest
-    document.getElementById("fire-start-game-btn").addEventListener("click", function (event) {
-        if (event.target.firstChild.data == "START A NEW GAME") {
-            event.target.firstChild.data = "FIRE!!";
-            resetGame();
-            return;
-        }
-        var beeAttackedIndex = getRandomMinMax(0, beeNest.length - 1);
-        var beeAttacked = beeNest[beeAttackedIndex];
-
-        beeAttacked.attacked();
-        beeAttacked.printStatus(printStatus);
-        if (!beeAttacked.isAlive()) {
-            removeItem(beeNest, beeAttackedIndex);
-            repaint();
-            if (beeAttacked.whoIAm() == "Queen") {
-                printStatus("Great job you destroy the Nest", "success");
-                printStatus("Press start to reset the game", "success");
-                event.target.firstChild.data = "START A NEW GAME";
-            }
-        }
-    });
 };
 
 domReady(initialize);
--- js/hive.js
+++ js/hive.js
@@ -1,5 +1,6 @@
 
-function Hive() {
+function Hive(canvasId) {
+    this.canvas = document.getElementById(canvasId);
     this.bees = [];
     this.population = {
         queens: 1,
@@ -41,7 +42,7 @@ Hive.prototype.createHive = function () {
 Hive.prototype.drawHexGrid = function (opts, c) {
 
     var alpha = opts.alpha || 1;
-    var color = opts.color || '#1e1e1e';
+    var color = opts.color || '#ff5900';
     var lineWidth = opts.lineWidth || 1;
     var radius = opts.radius || 20;
     var mapGridCanvas = c.getContext("2d");
@@ -100,4 +101,64 @@ Hive.prototype.mixBees = function (array) {
     return array;
 };
 
+Hive.prototype.getRandomMinMax = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+Hive.prototype.underAttack = function () {
 
+};
+Hive.prototype.removeItem = function (index) {
+    this.bees.splice(index, 1);
+};
+Hive.prototype.events = function () {
+    var _self = this;
+    //Manage the firing button to kill he Nest
+    document.getElementById("fire-start-game-btn").addEventListener("click", function (event) {
+        if (event.target.firstChild.data === "START A NEW GAME") {
+            event.target.firstChild.data = "FIRE!!";
+            _self.resetGame();
+            return;
+        }
+        
+        var beeAttackedIndex = _self.getRandomMinMax(0, _self.bees.length - 1);
+        var beeAttacked = _self.bees[beeAttackedIndex];
+
+        beeAttacked.attacked();
+        beeAttacked.printStatus(_self.printStatus);
+        if (!beeAttacked.isAlive()) {
+            _self.removeItem(beeAttackedIndex);
+            _self.render();
+            if (beeAttacked.whoIAm() === "Queen") {
+                _self.printStatus("Great job you destroy the Nest", "success");
+                _self.printStatus("Press start to reset the game", "success");
+                event.target.firstChild.data = "START A NEW GAME";
+            }
+        }
+    });
+};
+
+//Draw Game layout
+Hive.prototype.render = function () {
+    var context = this.drawHexGrid({}, this.canvas);
+    var beeArray = this.bees;
+    var length = this.bees.length;
+
+    for (var i = 0; i < length; i++) {
+        beeArray[i].render(context);
+    }
+};
+
+Hive.prototype.resetGame = function () {
+    this.createHive();
+    this.render();
+};
+
+Hive.prototype.printStatus = function (statusText, className) {
+    var logGame = document.getElementById("log-game");
+    var status = document.createElement("LI");
+    var statusNodeText = document.createTextNode(statusText);
+    status.appendChild(statusNodeText);
+    status.className = className;
+    logGame.appendChild(status);
+
+};
